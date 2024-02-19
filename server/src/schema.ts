b90359548bff8e2f63314f8adc6334845b9f5fe8
Benjamin Swanzey <swanzeyb2001@gmail.com>
2024-02-19 11:32:38 -0800

    Update perms

---
+++ server/src/schema.ts
@@ -0,0 +1,10 @@
+import { sqliteTable, text, integer, blob } from 'drizzle-orm/sqlite-core'
+
+// crypto.randomUUID()
+
+export const domFacebook = sqliteTable('dom:facebook', {
+  id: text('id').primaryKey(),
+  type: text('type', { enum: ['index', 'post'] }),
+  timestamp: integer('timestamp', { mode: 'timestamp_ms' }),
+  html: blob('html', { mode: 'buffer' }),
+})
