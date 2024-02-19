33f056700e1e344e6e8961a6e71dab476e23fd25
Benjamin Swanzey <swanzeyb2001@gmail.com>
2024-02-18 16:44:24 -0800

    Add MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import Table from './Table'
+
 export default function App() {
   return (
     <div>
-      <h1>Hello, world!</h1>
+      <Table />
     </div>
   )
 }
--- src/background.ts
+++ src/background.ts
@@ -1,1 +1,14 @@
-console.log('hi')
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  if (
+    changeInfo.status !== 'complete' ||
+    !tab.url?.includes('https://www.facebook.com/marketplace/category/vehicles')
+  )
+    return
+
+  chrome.scripting
+    .executeScript({
+      target: { tabId: tabId },
+      files: ['out/script.js'],
+    })
+    .then(() => console.log('script injected'))
+})
